33b42423be7a225eb16f302dd65658d255e0fec0
Johannes Donath <akkarin@evil-co.com>
2012-11-30 10:48:15 +0100

    Created PayPalException

---
+++ PayPalException.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the PayPal Library.
+ *
+ * The PayPal Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The PayPal Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace paypal;
+
+/**
+ * This exception occurs if something goes wrong.
+ * @author			Johannes "Akkarin" Donath
+ * @copyright			© Copyright 2012 Evil-Co <http://www.evil-co.com>
+ * @license			All rights reserved.
+ * @category			PayPal/
+ */
+class PayPalException extends Exception { }
+?>
